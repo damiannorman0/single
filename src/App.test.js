b5ba853e2626e92c9b134c47034b0f6d4d66021a
Damian Norman <damiannorman@gmail.com>
2020-04-17 12:12:30 -0400

    fixed test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import 'index.scss';
+import App from 'App';
+import {Provider} from 'react-redux';
+import configureStore from 'store';
+import {createBrowserHistory} from "history";
+import {Router, Route} from 'react-router-dom';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import { render } from '@testing-library/react';
+
+const history = createBrowserHistory();
+
+test('renders app', () => {
+	const app = (<React.StrictMode>
+		<Provider store={configureStore()}>
+			<Router history={history}>
+				<Route exact path="/" component={App}/>
+			</Router>
+		</Provider>
+	</React.StrictMode>,
+		document.getElementById('root'));
+
+	render(app);
 });
